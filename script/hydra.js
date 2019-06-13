cff613228fbfa0eae57ef08bf249dd50aac5a133
Denis Osipov <osipov_d@list.ru>
2019-06-13 16:40:56 +0500

    Add organism selectors

--- script/hydra.js
+++ script/hydra.js
@@ -31,12 +31,36 @@ var Nuclide = function(element, isotope) {
 };
 
 
+// ERICA's data
+var organismNames = ["Phytoplankton", "Zooplankton"];
+
+
 // Organisms fieldset
 
 var addOrganismButton = document.getElementById("add-organism");
 
+// Add organism selector right before "add" button
 var addOrganism = function() {
-    var newOrganism = document.createElement("select");
+
+    // Parent container
+    var newOrganism = document.createElement("div");
+    newOrganism.className = "organism";  // for styling
+
+    // Selector
+    var selector = document.createElement("select");
+    for (var i = 0; i < organismNames.length; i++) {
+        var option = document.createElement("option");
+        option.textContent = organismNames[i];
+        selector.appendChild(option);
+    }
+    newOrganism.appendChild(selector);
+
+    // Organism button
+    var button = document.createElement("button");
+    button.type = "button";
+    button.textContent = "^";
+    newOrganism.appendChild(button);
+    
     addOrganismButton.parentNode.insertBefore(newOrganism, addOrganismButton);
 };
 
