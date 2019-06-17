e1ea458090d767feae417fc9a297120928d38ea8
Denis Osipov <osipov_d@list.ru>
2019-06-17 12:45:09 +0500

    Fix path to sql.js

--- script/erica/erica.db.js
+++ script/erica/erica.db.js
@@ -7,6 +7,6 @@ with sql.js v1.0.0 (https://github.com/kripken/sql.js).
 
 //Open a blank database
 var db;
-initSqlJs({ locateFile: filename => `../dist/${filename}` }).then(function (SQL) {
+initSqlJs({ locateFile: filename => `./script/sql.js/${filename}` }).then(function (SQL) {
     db = new SQL.Database();
 });
