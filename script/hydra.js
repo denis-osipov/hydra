3686b8618e610e590b292e556eb119f34278b31c
Denis Osipov <osipov_d@list.ru>
2019-06-20 17:09:47 +0500

    Rewrite adding organisms and isotopes

--- script/hydra.js
+++ script/hydra.js
@@ -7,72 +7,38 @@ ERICA version 1.3.1.33
 
 */
 
-
-// Global active item
-var activeItem;
-
-
-// Create constructors
-
-var Habitat = function(media) {
-    this.media = media;
-    this.inhabitants = {};
+// Setting
+var Setting = function() {
+    this.isotopes = new Set();
+    this.organisms = new Set();
+    this.distributionCoefficients = [];
+    this.concentrationRatios = [];
+    this.media = [];
+    this.habitats = [];
+    this.occupancyFactors = [];
+    this.radiationWeightingFactors = [];
+    this.activityConcentrations = [];
+    this.percentageDryWeight = null;
 };
 
-Habitat.prototype.addData = function() {
-    if (activeItem instanceof Organism) {
-        this.inhabitants[activeItem.name] = activeItem.isotopes;
-    }
-    else if (activeItem instanceof Isotope) {
-        for (var inhabitant in this.inhabitants) {
-            var input = prompt("Enter " + activeItem.name + " activity in " +
-                                inhabitant + 
-                                " or leave empty field to keep current value");
-            if (input !== null && input !== "") {
-                this.inhabitants[inhabitant][activeItem.name] = parseFloat(input);
-            }
-        }
-        for (var medium in this.media) {
-            var activity = parseFloat(prompt("Enter " + activeItem.name + " activity in " + medium));
-            media[medium][activeItem.name] = activity;
-            }
-    }
+// Spatial and temporal series
+Setting.prototype.addSeries = function(setting, type) {};
 
-    // Reset
-    document.getElementById("input").style.cursor = "";
-    activeItem = null;
+// Add isotopes and organisms to setting
+Setting.prototype.addIsotope = function(isotope) {
+    this.isotopes.add(isotope);
 };
 
-var Organism = function(name) {
-    this.name = name;
-    this.isotopes = {};
-}
-
-var Isotope = function(name) {
-    this.name = name;
-}
-
-
-// Calculation function
-
-var calculate = function() {
-    var results = document.getElementById("results");
-    results.innerHTML += "<p>Calculation results</p>";
+Setting.prototype.addOrganism = function(organism) {
+    this.organisms.add(organism);
 };
+
+
+var setting = new Setting();
+
 
 // Add item selector right before target element (button)
-var addItemSelector = function(event, type) {
-
-    switch (type) {
-        case "organism":
-            var array = organismNames;
-            var constructor = Organism;
-            break;
-        case "isotope":
-            var array = isotopeNames;
-            var constructor = Isotope;
-            break;
-    }
+var addItemSelector = function(event, array) {
 
     // Parent container
     var newItemSelector = document.createElement("div");
@@ -92,9 +58,13 @@ var addItemSelector = function(event, type) {
     button.type = "button";
     button.textContent = "^";
     button.addEventListener("click", function(e) {
-        var itemName = e.target.previousSibling.value;
-        activeItem = new constructor(itemName);
-        document.getElementById("input").style.cursor = "cell";
+        var value = e.target.previousSibling.value;
+        if (event.target.id === "add-isotope") {
+            setting.addIsotope(value);
+        }
+        else {
+            setting.addOrganism(value);
+        }
     });
     newItemSelector.appendChild(button);
 
@@ -105,52 +75,11 @@ var addItemSelector = function(event, type) {
 // organisms fieldset
 var addOrganismButton = document.getElementById("add-organism");
 addOrganismButton.addEventListener("click", function(e){
-    addItemSelector(e, "organism")
+    addItemSelector(e, organisms)
 });
 
 // isotopes fieldset
 var addIsotopeButton = document.getElementById("add-isotope");
 addIsotopeButton.addEventListener("click", function(e){
-    addItemSelector(e, "isotope")
+    addItemSelector(e, isotopes)
 });
-
-
-// Media
-var media = {
-    water: {},
-    sediment: {}
-};
-
-// Habitats
-var waterSurface = new Habitat({water: 0.5});
-var water = new Habitat({water: 1.0});
-var sedimentSurface = new Habitat({water: 0.5, sediment: 0.5});
-var sediment = new Habitat({sediment: 0.5});
-
-var habitats = {
-    "water-surface": waterSurface,
-    "water": water,
-    "sediment-surface": sedimentSurface,
-    "sediment": sediment
-};
-
-var locations = document.getElementsByClassName("location");
-
-for (var i = 0; i < locations.length; i++) {
-    locations[i].addEventListener("click", function(e) {
-        habitats[e.target.id].addData();
-    });
-}
-
-var calcButton = document.getElementById("calculate");
-calcButton.addEventListener("click", calculate);
-
-
-// Setting
-var Setting = function() {
-    this.isotopes = [];
-    this.organisms = [];
-};
-
-// Spatial and temporal series
-Setting.prototype.series = function(setting, type) {};
