70a13e9aff92a964b575e4760c24af667c3ecbb9
Denis Osipov <osipov_d@list.ru>
2019-07-03 10:47:46 +0500

    Read date from csv

--- script/test.js
+++ script/test.js
@@ -3,3 +3,31 @@
 Compare Hydra's results with ERICA's results
 
 */
+
+var standardResults = {};
+
+var standardFile = document.getElementById("standard");
+
+var readTable = function() {
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.onload = function(event) {
+        var lines = this.result.split("\r\n");
+
+        // In ERICA's results organisms are in first row with first empty cell
+        var organisms = lines[0].split(";").slice(1);
+
+        var data = lines.slice(1);
+        for (row of data) {
+            var cells = row.split(";");
+            var isotope = cells[0];
+            standardResults[isotope] = {};
+            for (var i = 0; i < organisms.length; i++) {
+                standardResults[isotope][organisms[i]] = parseFloat(cells[i + 1]);
+            }
+        }
+    };
+    reader.readAsText(file);
+};
+
+standardFile.addEventListener("change", readTable);
